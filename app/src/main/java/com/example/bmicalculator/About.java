c288e5814df1c4301f18da6aa2ccd3f26481241f
fairunnasuha <fairunnasuhan1@gmail.com>
2022-01-19 11:15:26 +0800

    individualAssignment

--- app/src/main/java/com/example/bmicalculator/About.java
+++ app/src/main/java/com/example/bmicalculator/About.java
@@ -3,12 +3,22 @@ package com.example.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class About extends AppCompatActivity {
+    TextView git, tube, mail;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        git = findViewById(R.id.github);
+        git.setMovementMethod(LinkMovementMethod.getInstance());
+        tube = findViewById(R.id.youtube);
+        tube.setMovementMethod(LinkMovementMethod.getInstance());
+        mail = findViewById(R.id.email);
+        mail.setMovementMethod(LinkMovementMethod.getInstance());
 
     }
 }
